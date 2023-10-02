4c2057977fb8158950d38563c8b7aad0f36d5d54
perarahmawati <peraa.rahmawatii@gmail.com>
2023-10-02 09:22:12 +0700

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
     <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
     <link rel="icon" type="image/png" href="/img/favicon.png">
     <title>
-        Argon Dashboard 2 by Creative Tim
+        AMS
     </title>
-    <!--     Fonts and icons     -->
+
+    <!-- Fonts and icons -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Nucleo Icons -->
     <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
@@ -19,6 +20,7 @@
     <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- CSS Files -->
     <link id="pagestyle" href="assets/css/argon-dashboard.css" rel="stylesheet" />
+
 </head>
 
 <body class="{{ $class ?? '' }}">
